0ca1a9dc318d7e3526e16986293dfc06b54dfce8
watchdog1023 <Naickercd@gmail.com>
2018-03-05 16:36:49 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,5 @@
 @echo off
-cd "C:\Users\Cameron\Desktop\code projects\C Programming\Done\C++\pyversioncreater"
 g++ -v -fpermissive -c pyversioncreater.cpp -o pyversioncreater.o -Wwrite-strings -Wdeprecated-declarations -Wfatal-errors
 PAUSE
 g++ -v -static -static-libgcc -static-libstdc++ -o pyversioncreater.exe pyversioncreater.o
 rm -v *.o
-cd "C:\Users\Cameron\Desktop\code projects\C Programming\Done\C++\pyversioncreater"
